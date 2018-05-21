0150a487cec48e13d14fc9f43eae7f96cbbe3f92
Waco <wacol72@gmail.com>
2018-05-21 20:11:33 +0200

    add untracked files

--- readme.txt
+++
@@ -1,9 +0,0 @@
-Instrukcja obsługi:
-1) uruchom plik install_requirements.sh aby zainstalować wymagane pakiety(Windows - trzeba kliknąć ikonę aby uruchomić, Linux - wpisać w terminalu ./install_dependencies.sh)
-2) uruchom plik run.sh aby uruchomić aplikację(Windows - trzeba kliknąć ikonę aby uruchomić,
-	Linux - wpisać w terminalu ./run.sh)
-3) Aplikacja pozwala wprowadzać dane w takiej postaci:
-	54.35 14.65 - kolejno szerokość i długość geograficzna.
-	Dane mają postać dodatnią dla szerokości północnej i długości wschodniej.
-	Dane mają postać ujemną dla szerokości południowej i długości zachodniej.
-	np San Francisco : 37.77 -122.43
--- weather_app.py
+++ weather_app.py
@@ -4,15 +4,67 @@ window. Script is using OpenWeatherMaps API. It is necessary to
 use python3 due to displaying UTF signs properly.
 
 usage:
-	python3 weather_app.py
+    python3 weather_app.py
 """
 import webbrowser
 
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
 import requests
+from wtforms import DecimalField
+from wtforms.validators import InputRequired, ValidationError
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'teachairrisk'
+
+
+def check_latitude(form, field):
+    if not -80 < field.data < 80:
+        raise ValidationError('Latitude must be a number between -80 and 80')
+
+
+def check_longitude(form, field):
+    if not -180 < field.data < 180:
+        raise ValidationError('Longitude must be a number between -180 and 180')
+
+
+class CoordinatesForm(FlaskForm):
+    latitude_bottom = DecimalField('latitude_bottom', validators=[InputRequired(), check_latitude])
+    longitude_left = DecimalField('longitude_left', validators=[InputRequired(), check_longitude])
+    latitude_top = DecimalField('latitude_top', validators=[InputRequired(), check_latitude])
+    longitude_right = DecimalField('longitude_right', validators=[InputRequired(), check_longitude])
+
+
+def get_mean_temp(station_temp_dicterature):
+    """Get mean temperature.
+    Args:
+        station_temp_dicterature(dict): dict of pairs (station name : temperature)
+    Returns:
+        mean_temp(str): mean temperature of all stations results
+    """
+    temp_sum = sum(temp for temp in station_temp_dicterature.values())
+    mean_temp = temp_sum / len(station_temp_dicterature)
+    return '%.1f' % mean_temp
+
+
+def get_stations_temperatures(lon_left, lat_bottom, lon_right, lat_top):
+    """Function gets all stations in the area & their temperatures drawn by a rectangle,
+    which has opposite vertices in the points (lon_left, lat_bottom)
+    & (lon_right, lat_top). Uses openWeatherMaps API.
+    Args:
+        lon_left(float): longitude of the left bottom vertex
+        lat_bottom(float): latitude of the left bottom vertex
+        lon_right(float): longitude of the right top vertex
+        lat_top(float): latitude of the right top vertex
+    Returns:
+        station_temp_dicterature(dict) : dictionary with pairs (station_name : temperature)
+    """
+    response = requests.get('http://api.openweathermap.org/data/2.5/box/city?bbox={},{},{},{},10'
+                            '&appid=0a31749b47bc89f410b57d12a7cdee7b'.format(lon_left, lat_bottom, lon_right, lat_top))
+    json_response = response.json()
+    station_temp_dicterature = {item['name']: item['main']['temp'] for item in json_response['list']}
+    return station_temp_dicterature
 
 
 @app.route('/index', methods=['POST', 'GET'])
@@ -21,31 +73,20 @@ def index():
     Using open weather maps API gets temperature in Kelvin degrees
     & converts it to Celsius degrees. Then, renders it.
     """
-    if request.method == 'POST':
-        coordinates = request.form['data']
-        if coordinates == '' or not coordinates.isdigit():
-            return render_template('error.html')
-        try:
-            latitude, longitude = coordinates.split()
-            if -80 < float(latitude) < 80 or -180 < float(longitude) < 180:
-                return render_template('error.html')
-        except ValueError:
-            return render_template('error.html')
-        finally:
-            response = requests.get('http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}'
-                                   '&appid=0a31749b47bc89f410b57d12a7cdee7b'.format(latitude, longitude))
-            json_response = response.json()
-            station_name = str(json_response['name'])
-            kelvin_degrees = float(json_response['main']['temp'])
-            temperature = kelvin_degrees - 272.15
-            return render_template('result.html', name=station_name, temperature="{:.1f}".format(temperature))
-    elif request.method == 'GET':
-        return render_template('index.html')
+    form = CoordinatesForm()
+    if form.validate_on_submit():
+        station_temp_dicterature = get_stations_temperatures(form.longitude_left.data, form.latitude_bottom.data,
+                                                             form.longitude_right.data, form.latitude_top.data)
+        mean_temp = get_mean_temp(station_temp_dicterature)
+        return render_template('result.html', mean_temp=mean_temp,
+                               station_temp_dicterature=station_temp_dicterature)
+
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
     url = 'http://localhost:5000/index'
-    webbrowser.open(url, 1)
+    webbrowser.open_new_tab(url)
     app.run(debug=True)
 
 
